cd61bba1a6cb01d190791cdbe75b8a95335a8272
JVictor011 <jvictor2021@alu.ufc.br>
2025-01-20 15:46:10 -0300

    sem RabbitMq e WebSocket

--- src/main/java/com/JVictor011/maquina_de_turing/config/RabbitConfig.java
+++ src/main/java/com/JVictor011/maquina_de_turing/config/RabbitConfig.java
@@ -1,50 +1,50 @@
-package com.JVictor011.maquina_de_turing.config;
-
-import org.springframework.amqp.core.*;
-import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.rabbit.listener.RabbitListenerContainerFactory;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class RabbitConfig {
-
-    @Bean
-    public Queue turingMachineQueue() {
-        return new Queue("turing-machine-queue", true, false, false);
-    }
-
-    @Bean
-    public DirectExchange directExchange() {
-        return new DirectExchange("turing-exchange", true, false); // true = durable
-    }
-
-    @Bean
-    public Binding binding(Queue turingMachineQueue, DirectExchange directExchange) {
-        return BindingBuilder.bind(turingMachineQueue).to(directExchange).with("turing.routing.key");
-    }
-
-    @Bean
-    public Jackson2JsonMessageConverter jsonMessageConverter() {
-        return new Jackson2JsonMessageConverter();
-    }
-
-    @Bean
-    public RabbitListenerContainerFactory<?> rabbitListenerContainerFactory(ConnectionFactory connectionFactory,
-                                                                            Jackson2JsonMessageConverter jsonMessageConverter) {
-        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
-        factory.setConnectionFactory(connectionFactory);
-        factory.setMessageConverter(jsonMessageConverter);
-        return factory;
-    }
-
-    @Bean
-    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
-        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
-        rabbitTemplate.setMessageConverter(jsonMessageConverter());
-        return rabbitTemplate;
-    }
-}
+//package com.JVictor011.maquina_de_turing.config;
+//
+//import org.springframework.amqp.core.*;
+//import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+//import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+//import org.springframework.amqp.rabbit.core.RabbitTemplate;
+//import org.springframework.amqp.rabbit.listener.RabbitListenerContainerFactory;
+//import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class RabbitConfig {
+//
+//    @Bean
+//    public Queue turingMachineQueue() {
+//        return new Queue("turing-machine-queue", true, false, false);
+//    }
+//
+//    @Bean
+//    public DirectExchange directExchange() {
+//        return new DirectExchange("turing-exchange", true, false); // true = durable
+//    }
+//
+//    @Bean
+//    public Binding binding(Queue turingMachineQueue, DirectExchange directExchange) {
+//        return BindingBuilder.bind(turingMachineQueue).to(directExchange).with("turing.routing.key");
+//    }
+//
+//    @Bean
+//    public Jackson2JsonMessageConverter jsonMessageConverter() {
+//        return new Jackson2JsonMessageConverter();
+//    }
+//
+//    @Bean
+//    public RabbitListenerContainerFactory<?> rabbitListenerContainerFactory(ConnectionFactory connectionFactory,
+//                                                                            Jackson2JsonMessageConverter jsonMessageConverter) {
+//        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+//        factory.setConnectionFactory(connectionFactory);
+//        factory.setMessageConverter(jsonMessageConverter);
+//        return factory;
+//    }
+//
+//    @Bean
+//    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+//        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+//        rabbitTemplate.setMessageConverter(jsonMessageConverter());
+//        return rabbitTemplate;
+//    }
+//}
--- src/main/java/com/JVictor011/maquina_de_turing/service/WebSocketService.java
+++ src/main/java/com/JVictor011/maquina_de_turing/service/WebSocketService.java
@@ -1,20 +1,20 @@
-package com.JVictor011.maquina_de_turing.service;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.stereotype.Service;
-
-@Service
-public class WebSocketService {
-
-    private final SimpMessagingTemplate messagingTemplate;
-
-    @Autowired
-    public WebSocketService(SimpMessagingTemplate messagingTemplate) {
-        this.messagingTemplate = messagingTemplate;
-    }
-
-    public void sendMessageToFrontend(String destination, Object message) {
-        messagingTemplate.convertAndSend(destination, message);
-    }
-}
+//package com.JVictor011.maquina_de_turing.service;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.messaging.simp.SimpMessagingTemplate;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class WebSocketService {
+//
+//    private final SimpMessagingTemplate messagingTemplate;
+//
+//    @Autowired
+//    public WebSocketService(SimpMessagingTemplate messagingTemplate) {
+//        this.messagingTemplate = messagingTemplate;
+//    }
+//
+//    public void sendMessageToFrontend(String destination, Object message) {
+//        messagingTemplate.convertAndSend(destination, message);
+//    }
+//}
